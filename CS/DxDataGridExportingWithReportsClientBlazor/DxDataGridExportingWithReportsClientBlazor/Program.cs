e391ef5e0597dc7ae0968cdca138e560c85f1d64
vladimir.zabelin@devexpress.com <vladimir.zabelin@devexpress.com>
2021-04-15 22:47:27 +0300

    update 20.2.6 and net 5

--- CS/DxDataGridExportingWithReportsClientBlazor/DxDataGridExportingWithReportsClientBlazor/Program.cs
+++ CS/DxDataGridExportingWithReportsClientBlazor/DxDataGridExportingWithReportsClientBlazor/Program.cs
@@ -1,15 +1,23 @@
-﻿using Microsoft.AspNetCore.Blazor.Hosting;
+﻿using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.JSInterop;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace DxDataGridExportingWithReportsClientBlazor
 {
     public class Program
     {
-        public static void Main(string[] args) {
-            CreateHostBuilder(args).Build().Run();
-        }
+        public static async Task Main(string[] args) {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.Services.AddDevExpressBlazor();
 
-        public static IWebAssemblyHostBuilder CreateHostBuilder(string[] args) =>
-            BlazorWebAssemblyHost.CreateDefaultBuilder()
-                .UseBlazorStartup<Startup>();
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.RootComponents.Add<App>("#app");
+            var host = builder.Build();
+            var jsInterop = host.Services.GetRequiredService<IJSRuntime>();
+            await host.RunAsync();
+        }
     }
 }
