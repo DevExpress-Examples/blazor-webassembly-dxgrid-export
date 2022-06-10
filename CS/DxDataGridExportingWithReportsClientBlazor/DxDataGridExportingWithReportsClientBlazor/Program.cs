de451713f155f549209e72a28169a21e8c1dcfc7
Elena Khamlyuk <80813840+khamlyuk@users.noreply.github.com>
2022-06-10 14:30:48 +0300

    upd (#4)

--- CS/DxDataGridExportingWithReportsClientBlazor/DxDataGridExportingWithReportsClientBlazor/Program.cs
+++ CS/DxDataGridExportingWithReportsClientBlazor/DxDataGridExportingWithReportsClientBlazor/Program.cs
@@ -1,23 +1,15 @@
-﻿using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.JSInterop;
-using System;
-using System.Net.Http;
-using System.Threading.Tasks;
+﻿using DxDataGridExportingWithReportsClientBlazor;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-namespace DxDataGridExportingWithReportsClientBlazor
-{
-    public class Program
-    {
-        public static async Task Main(string[] args) {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.Services.AddDevExpressBlazor();
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-            builder.RootComponents.Add<App>("#app");
-            var host = builder.Build();
-            var jsInterop = host.Services.GetRequiredService<IJSRuntime>();
-            await host.RunAsync();
-        }
-    }
-}
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddDevExpressBlazor();
+builder.Services.Configure<DevExpress.Blazor.Configuration.GlobalOptions>(options => {
+    options.BootstrapVersion = DevExpress.Blazor.BootstrapVersion.v5;
+});
+
+await builder.Build().RunAsync();
