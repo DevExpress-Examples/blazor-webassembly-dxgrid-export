d5b2e8163f382049de74b250e5a2c6c9df2b6d65
SvetlanaMikheeva <80316217+SvetlanaMikheeva@users.noreply.github.com>
2022-12-26 16:13:17 +0400

    move example files

---
+++ CS/DataSourceWebApi/Controllers/ProductsController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Cors;
+using DevExtreme.AspNet.Mvc;
+using DevExtreme.AspNet.Data;
+using System.IO;
+using DataSourceWebApi.Models;
+using DataSourceWebApi.Services;
+
+namespace DataSourceWebApi.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    [EnableCors("AllowAll")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ExportHelper _eh;
+        private readonly NWINDContext _context;
+
+        public ProductsController(NWINDContext context, ExportHelper eh) {
+            _context = context;
+            _eh = eh;
+        }
+
+        // GET: api/Products
+        [HttpGet]
+        public object Products(DataSourceLoadOptions loadOptions) {
+            return DataSourceLoader.Load(_context.Products, loadOptions);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> ExportedDocument(DataSourceLoadOptions loadOptions, string format) {
+            loadOptions.Skip = 0;
+            loadOptions.Take = 0;
+            byte[] content = await _eh.ExportResult(DataSourceLoader.Load(_context.Products.ToList(), loadOptions), format);
+            string fileName = "DataGrid." + format;
+            string mimeType = "application/octet-stream";
+            return new FileStreamResult(new MemoryStream(content), mimeType) {
+                FileDownloadName = fileName
+            };           
+        }
+    }
+}
---
+++ CS/DataSourceWebApi/Models/NWINDContext.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace DataSourceWebApi.Models
+{
+    public partial class NWINDContext : DbContext
+    {
+        public NWINDContext() {
+        }
+
+        public NWINDContext(DbContextOptions<NWINDContext> options)
+            : base(options) {
+        }
+
+        public virtual DbSet<Products> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            if (!optionsBuilder.IsConfigured) {
+                optionsBuilder.UseSqlServer("Server=(local);Database=NWIND;User ID=XXX;Password=XXX;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            modelBuilder.Entity<Products>(entity => {
+                entity.HasKey(e => e.ProductId);
+
+                entity.Property(e => e.ProductId).HasColumnName("ProductID");
+
+                entity.Property(e => e.CategoryId).HasColumnName("CategoryID");
+
+                entity.Property(e => e.Ean13)
+                    .HasColumnName("EAN13")
+                    .HasColumnType("text");
+
+                entity.Property(e => e.ProductName)
+                    .IsRequired()
+                    .HasMaxLength(40);
+
+                entity.Property(e => e.QuantityPerUnit).HasMaxLength(20);
+
+                entity.Property(e => e.SupplierId).HasColumnName("SupplierID");
+
+                entity.Property(e => e.UnitPrice).HasColumnType("smallmoney");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
